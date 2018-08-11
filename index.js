0b4d16fe00a6ddf8a870e312302e41179caf94ed
unknown <savinraul1983@yahoo.com>
2018-08-11 14:06:32 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+
+//------------------------------------
+
+//@route   Get localhost:5000
+//desc     Test router
+//@access  Public
+app.get('/', (req,res) => {
+  res.send({ hi: 'there' });
+});
+
+//------------------------------------
+
+//Port
+const PORT  = process.env.PORT || 5000
+app.listen(PORT);
+
+//------------------------------------
