b121c715914e132de8de2f08c7424509ba79fdaf
unknown <savinraul1983@yahoo.com>
2018-08-23 13:15:46 +0100

    return acces token

--- index.js
+++ index.js
@@ -23,9 +23,16 @@ passport.use(new GoogleStrategy(
 
 //@route   Get localhost:5000/auth/google
 //desc     Test router
-//@access  ?
+//@access  Private
 app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email '] })
 );
+
+//------------------------------------]
+
+//@route   Get localhost:5000/auth/google
+//desc     Test router
+//@access  Private
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 //------------------------------------
 
