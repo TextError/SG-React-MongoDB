0cb50729667aceb6c3c8d63923067512eb0cea10
unknown <savinraul1983@yahoo.com>
2018-08-25 15:48:11 +0100

    Connected to MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 // const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+//------------------------------------
+
+//Connect to MongoDB
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB connected');
+    console.log(`Server is running on port: ${PORT}`);
+  })
+  .catch(err => console.log(err));
 
 //------------------------------------
 
