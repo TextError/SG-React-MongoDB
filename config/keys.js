151da2cc2a3d64787359c8024723e28d3a77a7f9
unknown <savinraul1983@yahoo.com>
2018-08-26 19:19:12 +0100

    Prod & Dev

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,9 @@
-module.exports = {
-  googleClientID: '149621132971-fb5720ao7mq004a4hb95le71tvq8bncg.apps.googleusercontent.com',
-  googleClientSecret: 'V_G8vCFOiR8aSZ2n6FMXO1ZE',
-  mongoURI: 'mongodb://sg-react-dev:Sg-react-dev27@ds125392.mlab.com:25392/sg-react-dev',
-  cookieKey: 'vtvavtEWITCJMtvEITcwRowv246vv385yv395vc25'  //Just press some buttons :)
-};
+//keys.js figure out what set of credentials to return
+
+if (process.env.NODE_ENV === 'production') {
+  //we are in production - return the prod set of keys
+  module.exports = require('./prod');
+} else {
+  //we are in development - return the dev set of keys
+  module.exports = require('./dev');
+}
