208c6991ead76f5017c57849bed69b4b99e7b16d
unknown <savinraul1983@yahoo.com>
2018-08-26 14:56:12 +0100

    Logout google user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,19 +3,37 @@ const passport = require('passport');
 
 module.exports = (app) => {
 
-//@route   Get localhost:5000/auth/google
-//desc     Test router
-//@access  Private
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email '] })
-);
+  //@route   Get localhost:5000/auth/google
+  //desc     Authenticate Google user
+  //@access  Private
+  app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email '] })
+  );
 
-//------------------------------------]
+  //------------------------------------
 
-//@route   Get localhost:5000/auth/google
-//desc     Test router
-//@access  Private
-app.get('/auth/google/callback', passport.authenticate('google'));
+  //@route   Get localhost:5000/auth/google
+  //desc     Authenticate Google user - Callback
+  //@access  Private
+  app.get('/auth/google/callback', passport.authenticate('google'));
 
-//------------------------------------
+  //------------------------------------
 
+  //@route   Get localhost:5000/api/logout
+  //desc     Logout user
+  //@access  Private
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.json({ logout: 'You successfully logged out'});
+  });
+
+  //------------------------------------
+
+  //@route   Get localhost:5000/api/current_user
+  //desc     Respone with current user
+  //@access  Private
+  app.get('/api/current_user', (req, res) =>{
+    res.json(req.user);
+  });
+
+  //------------------------------------
 }
