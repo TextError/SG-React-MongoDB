b136766a4455ca0f62da488150dc3a0ce8b69f87
unknown <savinraul1983@yahoo.com>
2018-08-26 18:19:57 +0100

    heroku

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '149621132971-fb5720ao7mq004a4hb95le71tvq8bncg.apps.googleusercontent.com',
+  googleClientSecret: 'V_G8vCFOiR8aSZ2n6FMXO1ZE',
+  mongoURI: 'mongodb://sg-react-dev:Sg-react-dev27@ds125392.mlab.com:25392/sg-react-dev',
+  cookieKey: 'vtvavtEWITCJMtvEITcwRowv246vv385yv395vc25'  //Just press some buttons :)
+};
