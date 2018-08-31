31ce6ae7deef31e10d9026681bc09724fab88714
unknown <savinraul1983@yahoo.com>
2018-08-31 11:12:29 +0100

    first component

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+//Rendering leyer control (React Router)
+//function component
+
+import React from 'react';
+
+const App = () => {
+  return(
+    <div>
+      Hi there!
+    </div>
+  );
+};
+
+export default App;
