593e91e296e24a47ded0b5478921a3b0c7677d90
unknown <savinraul1983@yahoo.com>
2018-09-04 18:04:06 +0100

    action creator

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 //Rendering leyer control (React Router)
 //function component
 
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 //Components
@@ -16,19 +16,28 @@ const Dasboard = () => <h2>Dasboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => {
-  return(
-    <div className='container'>
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/surveys' component={Dasboard} />
-          <Route path='/surveys/new' component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+
+
+class App extends Component {
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return(
+      <div className='container'>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/surveys' component={Dasboard} />
+            <Route path='/surveys/new' component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
 export default App;
