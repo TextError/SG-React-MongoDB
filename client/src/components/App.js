5ae86156d18bbb46df3ea8ef9fbe3a4ad97b0900
unknown <savinraul1983@yahoo.com>
2018-09-05 16:24:19 +0100

    current_user redux

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,8 @@
 
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 //Components
 import Header from './Header';
@@ -21,7 +23,7 @@ const Landing = () => <h2>Landing</h2>
 class App extends Component {
 
   componentDidMount() {
-    
+    this.props.fetchUser();
   }
 
   render() {
@@ -40,4 +42,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, actions)(App);
